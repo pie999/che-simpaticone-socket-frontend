529ee3ae0a70f28c01f3c620eb3ff6ce262718c8
pie999 <105122549+pie999@users.noreply.github.com>
2023-06-25 17:32:05 +0200

    prepare for deploy

--- socket.js
+++ socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-// "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  import.meta.VITE_ENV === "production" ? undefined : "http://localhost:3000";
+  import.meta.env.VITE_ENV === "pro"
+    ? import.meta.env.VITE_URL
+    : "http://localhost:3000";
 
 export const socket = io(URL);
