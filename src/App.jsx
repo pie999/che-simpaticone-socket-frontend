567249d19d6b450022e6a4fe93ef3045ee7aa059
pie999 <105122549+pie999@users.noreply.github.com>
2023-07-01 08:24:45 +0200

    Add length limit on nickname

--- src/App.jsx
+++ src/App.jsx
@@ -102,6 +102,7 @@ function App() {
           <label htmlFor="username">nickname</label>
           <input
             id="username"
+            maxLength={15}
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
